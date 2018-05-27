96159fb38455e4b46a703736b99853cda6d7bc11
HossamHassan47 <hossam.hassan@outlook.com>
2018-05-27 18:36:10 +0200

    Albums Activity

--- app/src/main/java/com/wordpress/hossamhassan47/musicplayer/activities/NowPlayingActivity.java
+++ app/src/main/java/com/wordpress/hossamhassan47/musicplayer/activities/NowPlayingActivity.java
@@ -2,6 +2,7 @@ package com.wordpress.hossamhassan47.musicplayer.activities;
 
 import android.content.ContentResolver;
 import android.database.Cursor;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -10,22 +11,27 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import com.wordpress.hossamhassan47.musicplayer.R;
 import com.wordpress.hossamhassan47.musicplayer.adapters.SongAdapter;
+import com.wordpress.hossamhassan47.musicplayer.helper.Utilities;
 import com.wordpress.hossamhassan47.musicplayer.model.Song;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 
-public class NowPlayingActivity extends AppCompatActivity {
+public class NowPlayingActivity extends AppCompatActivity  implements MediaPlayer.OnCompletionListener, SeekBar.OnSeekBarChangeListener{
 
     String albumTitle;
     private RecyclerView recyclerView;
     private SongAdapter songAdapter;
     public ArrayList<Song> songsList;
+
+    Utilities utils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +45,18 @@ public class NowPlayingActivity extends AppCompatActivity {
 
         fillSongsList();
 
+        // Mediaplayer
+        mp = new MediaPlayer();
+        utils = new Utilities();
+
+        btnPlay = (ImageView) findViewById(R.id.image_view_play);
+        btnPlay .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                playSong(0);
+            }
+        });
+
         // Get Recycler View
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view_playlist_songs);
 
@@ -50,7 +68,6 @@ public class NowPlayingActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(songAdapter);
-
 
         songAdapter.notifyDataSetChanged();
     }
@@ -94,4 +111,61 @@ public class NowPlayingActivity extends AppCompatActivity {
         }
     }
 
+    // Media Player
+    private MediaPlayer mp;
+    private ImageView btnPlay;
+
+    /**
+     * Function to play a song
+     * @param songIndex - index of song
+     * */
+    public void  playSong(int songIndex){
+        // Play song
+        try {
+            mp.reset();
+            mp.setDataSource(songsList.get(songIndex).getSongPath());
+            mp.prepare();
+            mp.start();
+
+            // Displaying Song title
+            //String songTitle = songsList.get(songIndex).get("songTitle");
+            //songTitleLabel.setText(songTitle);
+
+            // Changing Button Image to pause image
+            btnPlay.setImageResource(R.drawable.baseline_pause_circle_outline_white_48);
+
+            // set Progress bar values
+            //songProgressBar.setProgress(0);
+            //songProgressBar.setMax(100);
+
+            // Updating progress bar
+            //updateProgressBar();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
 }
