f2654ed9439a3b3716218f40de82d6a7b6bb60ef
HossamHassan47 <hossam.hassan@outlook.com>
2018-05-30 15:41:56 +0200

    Review comments fix

--- app/src/main/java/com/wordpress/hossamhassan47/musicplayer/activities/NowPlayingActivity.java
+++ app/src/main/java/com/wordpress/hossamhassan47/musicplayer/activities/NowPlayingActivity.java
@@ -9,11 +9,13 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
@@ -98,6 +100,8 @@ public class NowPlayingActivity extends AppCompatActivity
         setContentView(R.layout.activity_now_playing);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         albumTitle = getIntent().getExtras().getString("albumTitle", "");
         setTitle(albumTitle);
@@ -472,5 +476,17 @@ public class NowPlayingActivity extends AppCompatActivity
 
             mAudioManger.abandonAudioFocus(afChangeListener);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
